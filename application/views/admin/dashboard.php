c1bc0aff7b0f30b9f141344a244a65f40da58f1e
om7271prakash <om7271prakash@gmail.com>
2018-03-22 16:35:47 +0530

    add some designing to the articles dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -2,41 +2,44 @@
 <div style = "margin-top:6%"></div>
 <div class="container-fluid">
   <!-- Example DataTables Card-->
+
   <div class="card mb-3">
     <div class="card-header">
       <i class="fa fa-table"></i> Admin Panel Articles</div>
     <div class="card-body">
       <div class="table-responsive">
-        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>TITLE</th>
-              <th>ACTION</th>
-            </tr>
-          </thead>
-          <tfoot>
-            <tr>
-              <th>ID</th>
-              <th>TITLE</th>
-              <th>ACTION</th>
-            </tr>
-          </tfoot>
-          <tbody>
-            <?php if(count($articles)) { ?>
-              <?php foreach ($articles as $article) { ?>
-                <tr>
-                  <td><?= $article['id'] ?></td>
-                  <td><?= $article['title'] ?></td>
-                  <td>
-                    <a href="#" class="btn btn-primary">Edit</a>
-                    <a href="#" class="btn btn-danger">Delete</a>
-                  </td>
-                </tr>
+        <div class="col-md-10 offset-md-1">
+          <table class="table table-bordered admin-dashboard-articles-table" id="dataTable" width="100%" cellspacing="0">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>TITLE</th>
+                <th>ACTION</th>
+              </tr>
+            </thead>
+            <tfoot>
+              <tr>
+                <th>ID</th>
+                <th>TITLE</th>
+                <th>ACTION</th>
+              </tr>
+            </tfoot>
+            <tbody>
+              <?php if(count($articles)) { ?>
+                <?php foreach ($articles as $article) { ?>
+                  <tr>
+                    <td><?= $article['id'] ?></td>
+                    <td><?= $article['title'] ?></td>
+                    <td>
+                      <a href="#" class="btn btn-primary">Edit</a>
+                      <a href="#" class="btn btn-danger">Delete</a>
+                    </td>
+                  </tr>
+                <?php } ?>
               <?php } ?>
-            <?php } ?>
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
