d9e58a0e1a2e11b402bc8b0d002ba0c74c32a4d9
om7271prakash <om7271prakash@gmail.com>
2018-03-26 19:53:35 +0530

    pagination design has applied

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -21,9 +21,25 @@ class Admin extends MY_Controller{
 		$this->load->library('pagination');
 
 		$config = [
-								"base_url" => base_url('admin/dashboard'),
-								"per_page" => 5,
-								"total_rows" => $this->articles->num_rows()
+								"base_url" 				=> base_url('admin/dashboard'),
+								"per_page" 				=> 5,
+								"total_rows" 			=> $this->articles->num_rows(),
+								"full_tag_open" 	=> "<ul class = 'pagination'>",
+								"full_tag_close" 	=> "</ul>",
+								"first_tag_open" 	=> "<li  class='page-item page-link'>",
+								"first_tag_close" => "</li>",
+								"last_tag_open" 	=> "<li  class='page-item page-link'>",
+								"last_tag_close" 	=> "</li>",
+								"first_link"			=> "<<",
+								"last_link"			=> ">>",
+								"next_tag_open"	  => "<li  class='page-item page-link'>",
+								"next_tag_close" 	=> "</li>",
+								"prev_tag_open" 	=> "<li  class='page-item page-link'>",
+								"prev_tag_close" 	=> "</li>",
+								"num_tag_open" 		=> "<li  class='page-item page-link'>",
+								"num_tag_close" 	=> "</li>",
+								"cur_tag_open" 		=> "<li class='page-item active'><a class = 'page-link'>",
+								"cur_tag_close" 	=> "</a></li>"
 		];
 
 		$this->pagination->initialize($config);
